486df0e673fca3f8af48d8b8d35be4e3e5885e23
Aryan Raj Varma <141814661+Aryan-Raj-Varma@users.noreply.github.com>
2025-05-15 23:00:03 +0530

    Banking_system.h

--- Banking_system.h
+++ Banking_system.h
@@ -3,25 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
+#include <string.h>
+#include <time.h>
 
-#define MAX_ACCOUNTS 100
-#define MAX_TRANSACTIONS 5
 #define MAX_NAME_LENGTH 100
 #define MAX_CONTACT_LENGTH 15
-#define MAX_TRANSACTION_HISTORY 5
+#define MAX_TRANSACTIONS 100
 
-typedef enum {
-    DEPOSIT,
-    WITHDRAW
-} transaction_type;
+typedef enum { DEPOSIT, WITHDRAW } transaction_type;
 
 typedef struct {
     uint32_t transaction_id;
     transaction_type type;
     double amount;
-    char date[20];  // Simple string to store date
+    char date[20];
 } transaction;
 
 typedef struct account {
@@ -29,12 +25,11 @@ typedef struct account {
     char account_name[MAX_NAME_LENGTH];
     double account_balance;
     uint32_t account_transactions_count;
-    transaction transactions[MAX_TRANSACTION_HISTORY];
+    transaction transactions[MAX_TRANSACTIONS];
     char contact_number[MAX_CONTACT_LENGTH];
     struct account *next;
 } account;
 
-// Function declarations
 void Create_account(void);
 void Withdraw(void);
 void Deposit(void);
@@ -47,5 +42,6 @@ void Display_all_accounts(void);
 void Find_account(void);
 void Quit(void);
 void Display_menu(void);
+void Free_all_accounts(void);
 
-#endif /* BANKING_SYSTEM_H */
+#endif
